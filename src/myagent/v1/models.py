4294ac907328c6b236181e7e610bd6aa6d2e7455
marcoslashpro <tambascomarco35@gmail.com>
2026-02-28 14:36:29 +0100

    chore(Refactor): Moved rendering logic for system prompt in a standalone model

--- src/myagent/v1/models.py
+++ src/myagent/v1/models.py
@@ -45,3 +45,65 @@ class DockerSpecs:
 
 
 Volumes = dict[str, dict[Literal["bind", "mode"], str]]
+
+
+@dataclass(slots=True)
+class AllVolumes:
+    user_tools: Volumes
+    agent_tools: Volumes
+    docs: Volumes
+
+    def render_for_sys_prompt(self) -> str:
+        return f"""
+```bash
+/mnt/
+  {self._format_docs_for_sys_prompt(self.docs)}
+  {self._format_tools_for_sys_prompt(self.agent_tools)}
+  {self._format_tools_for_sys_prompt(self.user_tools)}
+```
+"""
+
+    def to_docker_volumes(self) -> dict:
+        return {**self.docs, **self.agent_tools, **self.user_tools}
+
+    def _format_docs_for_sys_prompt(self, docs: dict, level: int = 0):
+        formatted = ""
+        indent = "   " * level
+
+        for mnt in docs.keys():
+            local_p = Path(mnt)
+            bind_path = docs[mnt]["bind"]
+            mode = docs[mnt]["mode"]
+
+            if local_p.is_dir():
+                formatted += f"{indent}|-{bind_path}/  # {mode}\n"
+
+                for _, subdirs, filenames in local_p.walk():
+                    for file in filenames:
+                        file_indent = "   " * (level + 1)
+                        formatted += f"{file_indent}|-{file}  # {mode}\n"
+
+                    if subdirs:
+                        for subdir in subdirs:
+                            formatted += self._format_docs_for_sys_prompt(
+                                {
+                                    str(local_p / subdir): {
+                                        "mode": mode,
+                                        "bind": subdir,
+                                    }
+                                },
+                                level + 1,
+                            )
+                    break
+            else:
+                formatted += f"{indent}|-{bind_path}  # {mode}\n"
+
+        return formatted
+
+    def _format_tools_for_sys_prompt(self, tools: Volumes) -> str:
+        names = []
+        indent = "    "
+        for local_tool_path in tools.keys():
+            names.append(f"{Path(tools[local_tool_path]['bind'])}")
+
+        return f"\n{indent}|-".join(names) + "\n"
