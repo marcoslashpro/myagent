781a5db32e2fa90647c6c55926150cda417c045b
marcoslashpro <tambascomarco35@gmail.com>
2026-02-28 14:27:24 +0100

    chore(Refactor): Create standalone DockerConfiguration

---
+++ src/myagent/v1/environment/config.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from myagent.v1.errors import DockerConfigurationError
+from myagent.v1.models import DockerSpecs, Mount
+from myagent.v1.tools import Tool
+
+
+_ROOT_DIR_NAME = "~/.myagent"
+_LOCAL_AGENT_TOOLS_DIR = "tools"
+_SYS_PROMPT_PATH = Path(__file__).parent.parent.parent / "prompts/docker_agent_sys.txt"
+
+
+@dataclass(slots=True)
+class DockerConfiguration:
+    specs: DockerSpecs | None = field(default=None)
+    mnt_dir: str = field(default="/mnt")
+    mnt_tools_dir: str = field(default="/mnt/tools")
+    tools: list[Tool] = field(default_factory=list)
+    mounts: list[Mount] = field(default_factory=list)
+
+    @property
+    def LOCAL_ROOT_DIR(self) -> Path:
+        if not (_root_path := Path(_ROOT_DIR_NAME).expanduser()).exists():
+            _root_path.mkdir(exist_ok=True)
+
+        return _root_path
+
+    @property
+    def LOCAL_AGENT_TOOLS_DIR(self) -> Path:
+        if not (
+            _tools_path := self.LOCAL_ROOT_DIR.joinpath(_LOCAL_AGENT_TOOLS_DIR)
+        ).exists():
+            _tools_path.mkdir(exist_ok=True)
+
+        return _tools_path
+
+    @property
+    def SYS_PROMPT_PATH(self) -> Path:
+        if not (_sys_prompt_path := Path(_SYS_PROMPT_PATH)).exists():
+            raise DockerConfigurationError(
+                f"[FATAL ERROR] - Unable to locate system prompt at path: {_sys_prompt_path}, "
+                "please inform the library maintainers."
+            )
+        return _sys_prompt_path
